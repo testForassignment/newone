610620c8e4703884baf4f043036ba048a3b19162
sheetalrb <bsheetal325@gmail.com>
2019-01-08 22:17:56 +0530

    changes

--- assignment/README.md
+++ assignment/README.md
@@ -1,3 +1,5 @@
+# application.properties file is configured for server port as 8080
+
 # TO CLEAN BUILD APPLICATION
 
 mvnw clean install
--- assignment/src/main/java/com/uxpsystems/assignment/AssignmentApplication.java
+++ assignment/src/main/java/com/uxpsystems/assignment/AssignmentApplication.java
@@ -1,19 +1,53 @@
  package com.uxpsystems.assignment;
 
+import java.net.InetAddress;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 public class AssignmentApplication {
-	 
+	private static final Logger log = LoggerFactory.getLogger(AssignmentApplication.class);
+
+    private final Environment env;
+
+    public AssignmentApplication(Environment env) {
+        this.env = env;
+    }
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
+		SpringApplication app = new SpringApplication(AssignmentApplication.class);
+       
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            hostAddress,
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
+	   
 	}
 	
-	//@Override
 	public void run(String... arg0) throws Exception {
-
-	   }
+	}
 }
 
